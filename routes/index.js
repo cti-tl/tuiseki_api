e0cd5adf0ec113b87a4f2259097e698827a25ea5
cti1650 <cti1650@gmail.com>
2021-02-12 00:03:48 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,32 @@ router.get("/", function (req, res, next) {
 
 router.get("/api", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Content-Type", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   res.send({ name: "太郎" });
   console.log("get");
 });
 
 router.post("/api", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Content-Type", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   res.send({ name: "太郎" });
   console.log("get");
+});
+
+router.post("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Content-Type", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.send({ name: "太郎" });
+  console.log("プリフライト");
+});
+router.options("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Content-Type", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.send();
 });
 
 module.exports = router;
